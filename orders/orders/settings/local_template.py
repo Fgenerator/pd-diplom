62f02f45734b6255ed19c079e82a4b81ec69947c
Evgeny Ermakov <desteev@gmail.com>
2020-12-24 16:41:45 +0500

    Added template for local settings

---
+++ orders/orders/settings/local_template.py
@@ -0,0 +1,14 @@
+# from orders.settings.settings import *
+
+# SECURITY WARNING: keep the secret key used in production secret!
+# SECRET_KEY = ''
+
+# SECURITY WARNING: don't run with debug turned on in production!
+# DEBUG = True
+
+# EMAIL_HOST = 'smtp.mail.ru'
+# EMAIL_HOST_USER = ''
+# EMAIL_HOST_PASSWORD = ''
+# EMAIL_PORT = '465'
+# EMAIL_USE_SSL = True
+# SERVER_EMAIL = EMAIL_HOST_USER
